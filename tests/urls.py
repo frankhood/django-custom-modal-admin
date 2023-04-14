5e15fecdc3167653acb321640944b117c85a4263
Davide Ria <d.ria@frankhood.it>
2023-04-14 09:52:09 +0200

    Dropped python3.6 support

--- tests/urls.py
+++ tests/urls.py
@@ -1,9 +1,9 @@
 # -*- coding: utf-8 -*-
-from __future__ import unicode_literals, absolute_import
+from __future__ import absolute_import, unicode_literals
 
-from django.conf.urls import url, include
 from django.contrib import admin
+from django.urls import path
 
 urlpatterns = [
-    url(r'^admin/', admin.site.urls),
+    path("admin/", admin.site.urls),
 ]
