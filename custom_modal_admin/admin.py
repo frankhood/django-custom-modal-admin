7344a24b53456bf5cbc07c40f69ab1d497d567f1
Davide Ria <d.ria@frankhood.it>
2023-04-18 16:00:21 +0200

    Updated custom modal admin for django on server

--- custom_modal_admin/admin.py
+++ custom_modal_admin/admin.py
@@ -1,18 +1,17 @@
 # -*- coding: utf-8 -*-
+from django.conf import settings
 from django.contrib import admin
 
 
 class CustomModalAdmin(admin.ModelAdmin):
-
     class Media:
+        extra = "" if settings.DEBUG else ".min"
         js = [
-            "admin/js/vendor/jquery/jquery.js",
-            'admin/js/jquery.init.js',
+            "admin/js/vendor/jquery/jquery%s.js" % extra,
+            "admin/js/jquery.init.js",
             "js/jquery-ui.min.js",
-            'js/custom_modal_admin.js',
+            "js/custom_modal_admin.js",
         ]
         css = {
-            'all': (
-                "css/jquery-ui.css",
-            ),
+            "all": ("css/jquery-ui.css",),
         }
